1c706ecb6565733a4de4d1567f5cd042f5bb92c1
LeonardSuli <leonardsuli1991@gmail.com>
2024-05-20 18:33:25 +0200

    implementation crud index, show

---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+
+        <img src="{{ $post->cover_image }}" alt="">
+
+        <div>{{ $post->title }}</div>
+
+        <div>{{ $post->slug }}</div>
+
+    </div>
+@endsection
